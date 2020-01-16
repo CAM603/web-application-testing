12d000249725abbe45ddc48b47ca48ac79259493
CAM603 <cameronhawley@comcast.net>
2020-01-16 16:24:11 -0500

    added components

---
+++ src/components/Dashboard.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+const Dashboard = () => {
+
+    return (
+        <div>
+            <div>
+                <h2>Strike</h2>
+                <button>push</button>
+            </div>
+            <div>
+                <h2>Ball</h2>
+                <button>push</button>
+            </div>
+            <div>
+                <h2>Foul</h2>
+                <button>push</button>
+            </div>
+            <div>
+                <h2>Hit</h2>
+                <button>push</button>
+            </div>
+        </div>
+    )
+}
+
+export default Dashboard;
---
+++ src/components/Display.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Display = () => {
+
+    return (
+        <div>
+            <div>
+                <h2>Balls</h2>
+                <p>0</p>
+            </div>
+            <div>
+                <h2>Strikes</h2>
+                <p>0</p>
+            </div>
+        </div>
+    )
+}
+
+export default Display;
